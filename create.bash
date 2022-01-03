8910a1204b3cdada537ebe84e27c8fa750c50ff4
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2022-01-03 03:04:21 +0100

    Update create.bash

--- create.bash
+++ create.bash
@@ -10,7 +10,7 @@ sed -i 's/end/\n  root to: "welcome#index"\nend/g' config/routes.rb
 
 bundle add devise
 rails generate devise:install
-sed -i 's/end/\n  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }\nend/g' config/environments/development.rb
+sed -i 's/\nend/\n\n  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }\nend/g' config/environments/development.rb
 rails generate devise user
 rails db:migrate
 
