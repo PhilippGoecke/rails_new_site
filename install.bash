19046fbacf934b4ecf91fe1444bc06ffac7dcdff
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2022-01-03 02:48:50 +0100

    Create install.bash

---
+++ install.bash
@@ -0,0 +1,12 @@
+sudo apt install libsqlite3-dev
+
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt install -y nodejs
+
+npm -v
+
+npm install --global yarn
+
+yarn -v
+
+#npm uninstall -g yarn
