aba1567b1d000143f5f51f15627380bc3e57ca85
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2022-01-04 21:22:44 +0100

    Update install.bash

--- install.bash
+++ install.bash
@@ -2,7 +2,14 @@
 
 sudo apt install libsqlite3-dev
 
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+#curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+KEYRING=/usr/share/keyrings/nodesource.gpg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | sudo tee "$KEYRING" >/dev/null
+gpg --no-default-keyring --keyring "$KEYRING" --list-keys
+DISTRO="$(lsb_release -s -c)"
+echo "deb [signed-by=$KEYRING] https://deb.nodesource.com/node_16.x $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb-src [signed-by=$KEYRING] https://deb.nodesource.com/node_16.x $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+sudo apt update
 sudo apt install -y nodejs
 
 npm -v
