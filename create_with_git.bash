bf4aa141317347dcd8f460b5800343d313d33d6f
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2022-01-10 13:13:50 +0100

    Update create_with_git.bash

--- create_with_git.bash
+++ create_with_git.bash
@@ -5,6 +5,7 @@ git init .
 while IFS= read -r cmd; do
   echo "Command read: $cmd"
   $cmd
+
   git add .
   git commit -am "$cmd"
 done < <(curl -fsSL https://raw.githubusercontent.com/PhilippGoecke/rails_new_site/main/create.bash)
