758f8131829d7fe6eb9d73732358c7ced1f585a1
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2022-01-11 16:27:29 +0100

    Update create_with_git.bash

--- create_with_git.bash
+++ create_with_git.bash
@@ -6,7 +6,6 @@ set -o pipefail
 IFS=$'\n'
 
 git init
-sleep 1
 git add --all .
 git commit -m "Initial commit"
 
@@ -15,12 +14,7 @@ while IFS= read -r cmd; do
   echo "Command read: $cmd"
   bash -c "$cmd"
 
-  wait
-
   git add --all .
   git commit -am "${cmd//\"/\\\"}"
-
-  wait
-  sleep 1
 done < <(curl -fsSL https://raw.githubusercontent.com/PhilippGoecke/rails_new_site/main/create.bash)
 set +f
